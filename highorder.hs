32ed61e0793c53b68998ef5b55d4030f51a1400a
Pedro Pazello <pedropazello.rj@gmail.com>
2015-01-20 13:21:36 -0200

    high order programming ( )

---
+++ highorder.hs
@@ -0,0 +1,42 @@
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred x = compare 100 x
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+      | p x       = x : filter p xs
+      | otherwise = filter p xs
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+     where p x = x `mod` 3829 == 0
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15)
+                       (map chain [1..100]))
+
+-- only folds and horses
